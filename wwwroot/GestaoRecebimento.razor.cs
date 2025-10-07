aedc703df76248e97cdc4959f1bb56377013de17
Thauane007 <pereirathauane50@gmail.com>
2025-10-07 16:43:07 -0400

    Gestão Recebimento
    gerenciando o recebimento9

---
+++ wwwroot/GestaoRecebimento.razor.cs
@@ -0,0 +1,45 @@
+﻿
+.container - recebimento {
+    background - color: #111;
+    color: #fff;
+    padding: 2rem;
+    border - radius: 10px;
+    font - family: "Segoe UI", sans - serif;
+}
+
+.titulo {
+    text-align: center;
+margin - bottom: 2rem;
+color: #fff;
+}
+
+.tabela {
+    width: 100 %;
+border - collapse: collapse;
+background - color: #1f1f1f;
+    border - radius: 8px;
+overflow: hidden;
+}
+
+.tabela th
+{
+    background-color: #6C554F;
+    color: white;
+    padding: 12px;
+}
+
+.tabela td
+{
+    border: 1px solid #4C3C3B;
+    padding: 10px;
+    text-align: center;
+}
+
+.tabela tr:nth - child(even) {
+    background - color: #2c2c2c;
+}
+
+.tabela tr:nth - child(odd) {
+    background - color: #1a1a1a;
+}
+
