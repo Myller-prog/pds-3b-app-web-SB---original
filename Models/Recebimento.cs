358f841e3075e0b467764a8c857bcb5541ba84d2
Thauane007 <pereirathauane50@gmail.com>
2025-10-08 16:31:37 -0400

    programa
    código de programa

--- Models/Recebimento.cs
+++ Models/Recebimento.cs
@@ -1,13 +1,13 @@
 ﻿namespace AppWeb.Models
 {
-    public class GestaoRecebimento
+    public class Recebimento
     {
         public int Id { get; set; }
         public string? Pedido { get; set; }
         public string? Valor { get; set; }
-        public string? FormaPagamento { get; set; }
+        public string? FormaPagamentoRec { get; set; }
         public string? Comprovante { get; set; }
-        public string? Cliente { get; set; }
+        public string? ClienteRec { get; set; }
         
     }
 }
---
+++ Models/RecebimentoDAO.cs
@@ -0,0 +1,61 @@
+﻿using AppWeb.Configs;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace AppWeb.Models
+{
+    public class RecebimentoDAO
+    {
+        private readonly Conexao _conexao;
+        public RecebimentoDAO(Conexao conexao) // serve pra fornecedor também
+        {
+            _conexao = conexao;
+        }
+
+        public List<Recebimento> listarTodos()
+        {
+            var lista = new List<Recebimento>();
+
+            var comando = _conexao.CreateCommand("SELECT * FROM recebimentos;");
+            var leitor = comando.ExecuteReader();
+
+            while (leitor.Read()) // enquanto tiver condição, o while vai rodar
+            {
+                var recebimento = new Recebimento();
+                recebimento.Id = leitor.GetInt32("id_receb");
+                recebimento.Pedido = DAOHelper.GetString(leitor, "pedido_receb");
+                recebimento.Valor = DAOHelper.GetString(leitor, "valor_receb");
+                recebimento.FormaPagamentoRec = DAOHelper.GetString(leitor, "formaPag_receb");
+                recebimento.Comprovante = DAOHelper.GetString(leitor, "comprovante_receb");
+                recebimento.ClienteRec = DAOHelper.GetString(leitor, "cliente_receb");
+
+
+                lista.Add(recebimento);
+            }
+
+            return lista;
+        }
+
+        public void Inserir(Recebimento recebimento)
+        {
+            try
+            {
+                var comando = _conexao.CreateCommand("INSERT INTO recebimento VALUES (@_pedido, @_valor, @_formaPagamento, @_comprovante, @_cliente)");
+
+                comando.Parameters.AddWithValue("@_pedido", recebimento.Pedido);
+                comando.Parameters.AddWithValue("@_valor", recebimento.Valor);
+                comando.Parameters.AddWithValue("@_formaPagamento", recebimento.FormaPagamentoRec);
+                comando.Parameters.AddWithValue("@_comprovante", recebimento.Comprovante);
+                comando.Parameters.AddWithValue("@_cliente", recebimento.ClienteRec);
+
+
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+    }
+}
