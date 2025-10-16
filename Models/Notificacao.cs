8b6991b0974a03cac2ec9eedb6c25395d9bcc768
Thauane007 <pereirathauane50@gmail.com>
2025-10-16 10:53:23 -0400

    NotificacaoDAO criado
    Criei NotificacaoDAO

--- Models/Notificacao.cs
+++ Models/Notificacao.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
         public string? NumeroPedido { get; set; }
-        public string? NomeCliente { get; set; }
-        public string? Status { get; set; }
-        public decimal? Total { get; set; }
+        public string? NomeClientePed { get; set; }
+        public string? StatusPed { get; set; }
+        public decimal? TotalPed { get; set; }
 
     }
 }
