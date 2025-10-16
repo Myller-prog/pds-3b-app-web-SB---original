21722c2a48fe39d937313f44ff552ec27e0ae606
Thauane007 <pereirathauane50@gmail.com>
2025-10-16 10:56:22 -0400

    Dados notificacaoDAO
    adicionei os dados

--- Models/NotificacaoDAO.cs
+++ Models/NotificacaoDAO.cs
@@ -1,6 +1,60 @@
-﻿namespace AppWeb.Models
+﻿using AppWeb.Configs;
+
+namespace AppWeb.Models
 {
     public class NotificacaoDAO
     {
+        private readonly Conexao _conexao;
+        public NotificacaoDAO(Conexao conexao)
+        {
+            _conexao = conexao;
+        }
+
+
+        public List<Notificacao> listarTodos()
+        {
+            var lista = new List<Notificacao>();
+
+            var comando = _conexao.CreateCommand("SELECT * FROM notificacao;");
+            var leitor = comando.ExecuteReader();
+
+
+            while (leitor.Read())
+            {
+                var notificacao = new Notificacao();
+                notificacao.Id = leitor.GetInt32("id_noti");
+                notificacao.NumeroPedido = DAOHelper.GetString(leitor, "numero_pedido_noti");
+                notificacao.NomeClientePed = DAOHelper.GetString(leitor, "nome_cliente_noti");
+                notificacao.StatusPed = DAOHelper.GetString(leitor, "status_noti");
+                comando.Parameters.AddWithValue("@_total_noti", notificacao.TotalPed);
+
+
+                lista.Add(notificacao);
+            }
+
+            return lista;
+        }
+
+        public void Inserir(Notificacao notificacao)
+        {
+            try
+            {
+                var comando = _conexao.CreateCommand("INSERT INTO cliente VALUES (@_nome, @_telefone, @_email, @_cidade, @_estado)");
+
+                comando.Parameters.AddWithValue("@_numeroPedido", notificacao.NumeroPedido);
+                comando.Parameters.AddWithValue("@_nomeCliente", notificacao.NomeClientePed);
+                comando.Parameters.AddWithValue("@_status", notificacao.StatusPed);
+                comando.Parameters.AddWithValue("@_total", notificacao.TotalPed);
+
+
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
     }
 }
