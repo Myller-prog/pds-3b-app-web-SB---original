9c8c57332c987b32b5dc3301bdf2acba99173aaa
mazcat <mazcat008@gmail.com>
2025-10-21 15:06:53 -0400

    produto
    coloquei os botões do  atualizar e excluir. a tela não entra, mas ta atualizando

--- Models/ProdutoDAO.cs
+++ Models/ProdutoDAO.cs
@@ -73,6 +73,53 @@ namespace AppWeb.Models
             }
 
         }
+        public Produto? BuscarPorId(int id)
+        {
+            var comando = _conexao.CreateCommand(
+                "SELECT * FROM produto WHERE id_pro = @id;");
+            comando.Parameters.AddWithValue("@id", id);
+
+            var leitor = comando.ExecuteReader();
+
+            if (leitor.Read())
+            {
+                var produto = new Produto();
+                produto.Id = leitor.GetInt32("id_pro");
+                produto.Nome = DAOHelper.GetString(leitor, "nome_pro");
+                produto.Descricao = DAOHelper.GetString(leitor, "descricao_pro");
+                produto.Quantidade = leitor.GetInt32("quantidade_pro");
+                produto.Valor = leitor.GetDecimal("preco_pro");
+
+                return produto;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void Atualizar(Produto produto)
+        {
+            try
+            {
+                var comando = _conexao.CreateCommand(
+                    "UPDATE produto SET nome_pro = @_nome, descricao_pro = @_descricao, " +
+                    "quantidade_pro = @_quantidade, preco_pro = @_preco WHERE id_pro = @_id;");
+
+                comando.Parameters.AddWithValue("@_nome", produto.Nome);
+                comando.Parameters.AddWithValue("@_descricao", produto.Descricao);
+                comando.Parameters.AddWithValue("@_quantidade", produto.Quantidade);
+                comando.Parameters.AddWithValue("@_preco", produto.Valor);
+                comando.Parameters.AddWithValue("@_id", produto.Id);
+
+                comando.ExecuteNonQuery();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+
 
     }
 
